71f018be4a43d0337e35086a0fab261d960e8db8
171547080 <171547080@qq.com>
2022-01-07 14:27:21 +0800

    添加Config插件

---
+++ demo/src/config/index.ts
@@ -0,0 +1,9 @@
+import Config from "react-native-config"
+
+const result = {
+    apiUrl:Config.API_URL,
+    version:Config.VERSION
+
+}
+
+export default result
---
+++ demo/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+class App extends React.Component{
+    render(){
+        return (
+            <View>
+                <Text>INDEX !!!</Text>
+            </View>
+        )
+    }
+}
